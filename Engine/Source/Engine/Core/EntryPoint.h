81e521f9c9d29de54a62d328dc7b0783553da5a8
Muerte <74092032+Muerte0-0@users.noreply.github.com>
2026-03-21 09:01:55 +0530

    update glfw build includes

--- Engine/Source/Engine/Core/EntryPoint.h
+++ Engine/Source/Engine/Core/EntryPoint.h
@@ -1,36 +1,15 @@
 ﻿#pragma once
 #include  "PlatformDetection.h"
 
-#define GLFW_INCLUDE_NONE
-#include <GLFW/glfw3.h>
+inline bool g_ApplicationRunning = true;
 
 inline int Main(int argc, char** argv)
 {
-	if (!glfwInit())
-		return -1;
-
-	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
 	
-	GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", nullptr, nullptr);
-	
-	if (!window)
-	{
-		glfwTerminate();
-		return -1;
-	}
-
-	while (!glfwWindowShouldClose(window))
-	{
-		glfwPollEvents();
-	}
-
-	glfwTerminate();
 	return 0;
 }
 
 #ifdef PLATFORM_WINDOWS
-
-inline bool g_ApplicationRunning = true;
 
 #ifdef KE_DIST
 
